6f014f92422bd3fb4f8feed5f6e7564c34eb0d99
Perris Davis <wikipediabrown@users.noreply.github.com>
2017-02-02 17:49:39 -0800

    Updated get customer function

--- Create Customer/function.js
+++ Create Customer/function.js
@@ -18,50 +18,85 @@ const datastore = Datastore({
 
 exports.createCustomer = function createCustomer(req, res) {
 
-  var idToken = req.body.idToken;
+var idToken = req.body.idToken
 
-    admin.auth().verifyIdToken(idToken).then(function(decodedToken) {
+  admin.auth().verifyIdToken(idToken).then(function(decodedToken) {
 
-      const userKey = datastore.key(['user', decodedToken.uid]);
+    const userKey = datastore.key(['user', decodedToken.uid]);
 
-      datastore.get(userKey).then((results) => {
-        if (typeof results[0] === 'undefined') {
-          return Promise.resolve(results)
-        } else {
-          Promise.reject(error)
-        }
-      }).then(function(results) {
+    datastore.get(userKey).then((results) => {
 
-        stripe.customers.create({
-          description: decodedToken.uid,
-        }, function(err, customer) {
+      if (typeof results[0] === 'undefined') {
+        return Promise.resolve(results)
+      } else {
+        return Promise.reject(results[0].customerID)
+      }
 
-          if (err == null) {
-            const entity = {
-                key: userKey,
-                data: {
-                  customerID: customer.id
-                }
-            };
-            datastore.upsert(entity).then(() => {
-              res.send(customer.id);
-            }).catch(function(error) {
-              res.send('Error saving user. Please try again.' + error);
-            });
+    }).then(function(results) {
 
-          } else {
-              res.send('Error creating user. Please try again.' + err);
+      stripe.customers.create({
+        description: decodedToken.uid,
+      }).then(function(customer) {
+
+        const entity = {
+          key: userKey,
+          data: {
+            customerID: customer.id
           }
+        };
 
+        datastore.upsert(entity).then(() => {
+          var customerObject = {};
+          if (customer.shipping !== null) {
+            customerObject['name'] = customer.shipping.name
+            customerObject['phone'] = customer.shipping.phone
+          }
+          if (customer.shipping !== null && customer.shipping.address !== null) {
+            customerObject['name'] = customer.shipping.name
+            customerObject['line1'] = customer.shipping.address.line1
+            customerObject['line2'] = customer.shipping.address.line2
+            customerObject['city'] = customer.shipping.address.city
+            customerObject['postalCode'] = customer.shipping.address.postal_code
+            customerObject['state'] = customer.shipping.address.state
+            customerObject['country'] = customer.shipping.address.country
+          }
+          customerObject['email'] = customer.email
+          res.json(customerObject)
+        }).catch(function(error) {
+          res.send('Error saving user. Please try again.' + error);
         });
 
-    }).catch(function() {
-        res.send('User already has account')
+      }).catch(function(error) {
+        res.send('Error creating user. Please try again.' + err);
+      });
+
+    }).catch(function(customerID) {
+
+      stripe.customers.retrieve(customerID).then(function(customer) {
+        var customerObject = {};
+
+        if (customer.shipping !== null) {
+          customerObject['name'] = customer.shipping.name
+          customerObject['phone'] = customer.shipping.phone
+        }
+        if (customer.shipping !== null && customer.shipping.address !== null) {
+          customerObject['name'] = customer.shipping.name
+          customerObject['line1'] = customer.shipping.address.line1
+          customerObject['line2'] = customer.shipping.address.line2
+          customerObject['city'] = customer.shipping.address.city
+          customerObject['postalCode'] = customer.shipping.address.postal_code
+          customerObject['state'] = customer.shipping.address.state
+          customerObject['country'] = customer.shipping.address.country
+        }
+        customerObject['email'] = customer.email
+        res.json(customerObject)
+      }).catch(function(error) {
+        res.send('User does not have a Stripe account'+error)
+      });
     });
 
-
   }).catch(function(error) {
-        res.send('Hey, who are you?')
+    res.send('Nigga, you aint real...')
   });
 };
 
